96a80cf098aeddcf2a919fd88a661ee0395664c3
bochaco <gabrielviganotti@gmail.com>
2018-12-28 17:06:17 -0300

    feat: automatically remove safe:// from the text entered in the WebID search box

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -16,7 +16,7 @@ class HeaderComponent extends React.Component
 
   onChangeSearchUri = ( e ) =>
   {
-      this.setState( { searchWebIdUri: e.target.value.replace(/^safe:\/\//i, '') } );
+      this.setState( { searchWebIdUri: e.target.value.trim().replace(/^safe:\/\//i, '') } );
   }
 
   handleSearch = async ( webIdUri ) =>
